a43ed362babdf614f96c614ff97cd2697b0502ba
dmjcb <dmjcb@outlook.com>
2024-10-27 02:10:58 +0800

    清理

--- _posts/Tool/Code/CleanBlogUnusedResource.py
+++ _posts/Tool/Code/CleanBlogUnusedResource.py
@@ -4,20 +4,18 @@ import codecs
 import sys
 import subprocess
 
-class CleanUnusedResource:
+class CleanBlogUnusedResource:
     __SYSTEM_PATH            = "c:\\Users\\dmjcb\\Documents\\Code"
     __IMGUR_PATH             = "assets\\SelfImgur"
     
     __SELF_BLOG_NAME         = "SelfBlog"
     __SELF_IO_NAME           = "dmjcb.github.io"
 
-    __SELF_BLOG_DIR = "{0}\\{1}".format(__SYSTEM_PATH, __SELF_BLOG_NAME)
-    __SELE_IO_DIR = "{0}\\{1}".format(__SYSTEM_PATH, __SELF_IO_NAME)
+    __SELF_BLOG_DIR          = "{0}\\{1}".format(__SYSTEM_PATH, __SELF_BLOG_NAME)
+    __SELE_IO_DIR            = "{0}\\{1}".format(__SYSTEM_PATH, __SELF_IO_NAME)
 
-    __SELF_BLOG_IMGUR_DIR   = "{0}\\{1}".format(__SELF_BLOG_DIR, __IMGUR_PATH)
-    __SELF_IO_IMGUR_DIR = "{0}\\{1}".format(__SELE_IO_DIR, __IMGUR_PATH)
-
-    used_iamge_count         = 0
+    __SELF_BLOG_IMGUR_DIR    = "{0}\\{1}".format(__SELF_BLOG_DIR, __IMGUR_PATH)
+    __SELF_IO_IMGUR_DIR      = "{0}\\{1}".format(__SELE_IO_DIR, __IMGUR_PATH)
 
 
     # 从SelfBlog所有.md中获取图片url, 提取图片名
@@ -37,7 +35,7 @@ class CleanUnusedResource:
                                     line = line[:-1]
                                     image_name = line.split('/')[-1]
                                     used_images.append(image_name)
-                                    self.used_iamge_count += 1
+
         return used_images
 
 
@@ -64,15 +62,19 @@ class CleanUnusedResource:
                     name = os.path.join(fpathe, f)
                     os.remove(name)
 
-        print("SelfBlog SelfImfur del {0} imges".format(del_count))
+        print("1. SelfBlog SelfImgur del {0} images".format(del_count))
 
 
     def copy_self_imgur_dir(self):
+        copy_count = 0
         src_files = os.listdir(self.__SELF_BLOG_IMGUR_DIR)
         for file_name in src_files:
             full_file_name = os.path.join(self.__SELF_BLOG_IMGUR_DIR, file_name)
             if os.path.isfile(full_file_name):
                 shutil.copy(full_file_name, self.__SELF_IO_IMGUR_DIR)
+                copy_count += 1
+        
+        print("2. Copy {0} images".format(copy_count))
 
     # 删除dmjcb.github.io/assets/SelfImgur中未使用的图片
     def del_io_imgur_unused_files(self):
@@ -86,7 +88,7 @@ class CleanUnusedResource:
                     os.remove(name)
                     del_count += 1
 
-        print("SelfBlog SelfImfur del {0} imges".format(del_count))
+        print("3. dmjgb.github.io SelfImgur del {0} images".format(del_count))
 
 
     def del_and_copy_image(self):
@@ -96,23 +98,34 @@ class CleanUnusedResource:
 
 
     def git_pipline(self):
+        print("4. run git")
+        def __run(command):
+            r = subprocess.run(command, shell=True, capture_output=True, text=True, encoding="utf8")
+            print(r.stdout)
+            print()
+        
+        def __pull():
+            sh = "git pull"
+            __run(sh)
+
+        def __push():
+            sh = "git add . && git commit -m {0} && git push".format(sys.argv[1])
+            __run(sh)
+        
         os.chdir(self.__SELF_BLOG_DIR)
-        pipline = ["git add .", "git commit -m {0}".format(sys.argv[1])]
-        for sh in pipline:
-            r = subprocess.run(sh, shell=True, capture_output=True, text=True)
+        __push()
 
         os.chdir("{0}\\_posts".format(self.__SELE_IO_DIR))
-        r = subprocess.run("git pull", shell=True, capture_output=True, text=True)
+        __pull()
 
         os.chdir(self.__SELE_IO_DIR)
-        pipline = ["git add .", "git commit -m {0}".format(sys.argv[1])]
-        for sh in pipline:
-            r = subprocess.run(sh, shell=True, capture_output=True, text=True)
+        __push()
+
 
     def run(self):
-        # self.del_and_copy_image()
+        self.del_and_copy_image()
         self.git_pipline()
 
 
-clean = CleanUnusedResource()
+clean = CleanBlogUnusedResource()
 clean.run()
